7499d8f83639a9bd4f657154fc98c17c2706d1d6
dineshdsv3 <dineshdsv3@gmail.com>
2020-10-05 19:25:13 +0530

    dev Setup, fetching data from server

--- backend/server.js
+++ backend/server.js
@@ -6,13 +6,13 @@ app.get('/', (req, res) => {
 	res.send('API is running');
 });
 
-app.get('/products', (req, res) => {
+app.get('/api/products', (req, res) => {
 	res.json(products);
 });
 
-app.get('/products/:id', (req, res) => {
+app.get('/api/products/:id', (req, res) => {
 	const product = products.find((p) => p._id === req.params.id);
 	res.json(product);
 });
 
-app.listen(5000, console.log('Server Running on Port 5000'));
+app.listen(7000, console.log('Server Running on Port 5000'));
