8cf2e94dc0bcd95e958cecd1b2757db21869877d
dineshdsv3 <dineshdsv3@gmail.com>
2020-10-05 19:32:53 +0530

    Added .env

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,9 @@
 const express = require('express');
+const dotenv = require('dotenv');
 const products = require('./data/products');
+
+dotenv.config();
+
 const app = express();
 
 app.get('/', (req, res) => {
@@ -15,4 +19,6 @@ app.get('/api/products/:id', (req, res) => {
 	res.json(product);
 });
 
-app.listen(7000, console.log('Server Running on Port 5000'));
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, console.log(`Server Running in ${process.env.NODE_ENV} mode on Port ${PORT}`));
